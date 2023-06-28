5d44647316644d6cebed8d1c327ca68ab22114df
lyonbett <lyon.bett@student.moringaschool.com>
2023-06-28 03:56:59 +0300

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "John", last_name: "Dear", grade: 4 )
+Student.create(first_name: "Jane", last_name: "Kia", grade: 7)
+Student.create(first_name: "Mary", last_name: "Kamau", grade: 5)
